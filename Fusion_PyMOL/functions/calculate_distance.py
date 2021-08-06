3b0c4505a3e6ad0a1afb531f2ea85206b36dcab5
Pudžiuvelytė Ieva <ieva.pudziuvelyte@mif.stud.vu.lt>
2021-08-06 13:14:23 +0300

    Calculations adjusted for several states

--- Fusion_PyMOL/functions/calculate_distance.py
+++ Fusion_PyMOL/functions/calculate_distance.py
@@ -5,10 +5,16 @@ from functions import center_of_mass
 
 # Subroutine that calculates distance between active sites when rigid linker
 # is used in the fusion protein system
-def calculate_distance(par):
-    center_of_mass.com('as_1_1', 1, 'COM_1')
-    center_of_mass.com('as_2_1', 1, 'COM_2')
-    cmd.distance('distance_COM', 'as_1_1_COM', 'as_2_1_COM')
+def calculate_distance(par, state=1):
+    COM_1 = 'COM_1_'+str(state)
+    COM_2 = 'COM_2_'+str(state)
+    center_of_mass.com('as_1_1', None, None, COM_1, 0)
+    center_of_mass.com('as_2_1', None, None, COM_2, 0)
+    cmd.distance('distance_COM', COM_1, COM_2)
+    cmd.create('ov_COM_1', COM_1, 0, state)
+    cmd.create('ov_COM_2', COM_2, 0, state)
+    cmd.delete(COM_1)
+    cmd.delete(COM_2)
     
     cmd.set('label_color', par.colors[0], 'distance_COM')
     cmd.color(par.colors[4], 'distance_COM')
