e1a70ba7be5ca81b38c5e02af35c1535b1274d3f
Pudžiuvelytė Ieva <ieva.pudziuvelyte@mif.stud.vu.lt>
2021-08-18 14:38:28 +0300

    Code adjusted to calculate one and multiple states distances

--- Fusion_PyMOL/main.py
+++ Fusion_PyMOL/main.py
@@ -1,7 +1,9 @@
 #!/usr/bin/env python
 import pymol
-from pathlib import Path
+#from pathlib import Path
 from pymol import cmd
+import os
+import subprocess
 
 from functions import parse_parameters, color_fusion, calculate_distance
 
@@ -19,9 +21,22 @@ par.obj_name = 'fusion'
 
 # Perform coloring
 parse_parameters.parse_parameters(sys.argv[1], par)
+
 color_fusion.color_fusion(par)
+
 # Structural alignment with homologs
 # color_fusion.load_templates(par)
+
 # Calculate distance between active sites
-for i in range(1, 100):
-    calculate_distance.calculate_distance(par, i)
+command = "grep 'MODEL' "+ par.fusion_file + " | wc -l"
+states = int(subprocess.getstatusoutput(command)[1])
+dist = [None] * states
+
+if states > 1:
+    print("Multiple states")
+    calculate_distance.calculate_distance_multiple_states(par, dist)
+else:
+    print("One state")
+    calculate_distance.calculate_distance(par, dist)
+
+calculate_distance.write_to_file(par, dist, "./tmp/distances.txt")
