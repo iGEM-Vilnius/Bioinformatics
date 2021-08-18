bc7d23196e7c3a44e5adf40fc2920eaa1987335e
Pudžiuvelytė Ieva <ieva.pudziuvelyte@mif.stud.vu.lt>
2021-08-18 14:39:20 +0300

    Added function to calculate distance for multiple state structures (models)

--- Fusion_PyMOL/functions/calculate_distance.py
+++ Fusion_PyMOL/functions/calculate_distance.py
@@ -2,19 +2,82 @@
 import pymol
 from pymol import cmd
 from functions import center_of_mass
+from pymol import stored
+from statistics import mean
+
+# Subroutine that counts how many digits the number has
+def countDigit(n):
+    count = 0
+    while n != 0:
+        n //= 10
+        count += 1
+    return count
 
 # Subroutine that calculates distance between active sites when rigid linker
 # is used in the fusion protein system
-def calculate_distance(par, state=1):
-    COM_1 = 'COM_1_'+str(state)
-    COM_2 = 'COM_2_'+str(state)
+def calculate_distance(par, dist):
+    COM_1 = 'COM_1_1'
+    COM_2 = 'COM_2_1'
+
+    # Calculating center of mass for each active site
     center_of_mass.com('as_1_1', None, None, COM_1, 0)
     center_of_mass.com('as_2_1', None, None, COM_2, 0)
-    cmd.distance('distance_COM', COM_1, COM_2)
-    cmd.create('ov_COM_1', COM_1, 0, state)
-    cmd.create('ov_COM_2', COM_2, 0, state)
-    cmd.delete(COM_1)
-    cmd.delete(COM_2)
+    
+    # Creating a distance object for visualisation in PyMOL
+    d = cmd.distance('distance_COM', COM_1, COM_2)
+    dist[0] = d
+    
+    # Creating a pseudo atom for distance calculation between atoms
+#    cmd.create('ov_COM_1', COM_1, 0, state)
+#    cmd.create('ov_COM_2', COM_2, 0, state)
+#    cmd.delete(COM_1)
+#    cmd.delete(COM_2)
     
     cmd.set('label_color', par.colors[0], 'distance_COM')
     cmd.color(par.colors[4], 'distance_COM')
+    
+    cmd.center(par.obj_name)
+    cmd.zoom(par.obj_name)
+    
+def calculate_distance_multiple_states(par, dist):
+    COM_1 = 'COM_1_1'
+    COM_2 = 'COM_2_1'
+
+    # Calculating center of mass for each active site
+    center_of_mass.com('as_1_1', None, None, COM_1, 0)
+    center_of_mass.com('as_2_1', None, None, COM_2, 0)
+    
+    # Creating a distance object for visualisation in PyMOL
+    d = cmd.distance('distance_COM', COM_1, COM_2)
+    
+    # Creating a pseudo atom for distance calculation between atoms
+    cmd.create('ov_COM_1', COM_1, 0, 1)
+    cmd.create('ov_COM_2', COM_2, 0, 1)
+    cmd.delete(COM_1)
+    cmd.delete(COM_2)
+    
+    # Calculating distance between pseudo atoms in each state
+    for i in range(1, len(dist)+1):
+        if len(str(i)) > 2:
+            num = str(i)[-2:]
+            dist[i-1] = cmd.get_distance(atom1="/ov_COM_1/PSDO/P/PSD`1/"+str(i)[0:len(str(i))-2]+"PS"+num, atom2="/ov_COM_2/PSDO/P/PSD`1/"+str(i)[0:len(str(i))-2]+"PS"+num, state=i)
+        else:
+            dist[i-1] = cmd.get_distance(atom1="/ov_COM_1/PSDO/P/PSD`1/PS"+str(i), atom2="/ov_COM_2/PSDO/P/PSD`1/PS"+str(i), state=i)
+    
+    cmd.set('label_color', par.colors[0], 'distance_COM')
+    cmd.color(par.colors[4], 'distance_COM')
+    
+    cmd.center(par.obj_name)
+    cmd.zoom(par.obj_name)
+
+def write_to_file(par, dist, file):
+    outFile = open(file, 'w')
+    
+    outFile.write("%s\n" % par.fusion_file)
+    outFile.write("Minimum distance: %s\n" % (min(dist)))
+    outFile.write("Maximum distance: %s\n" % (max(dist)))
+    outFile.write("Average distance: %s\n" % (mean(dist)))
+    
+    outFile.close()
+        
+    
