561b45fe5591fe6d0cb57577bacf9f4e46ad9dcf
Pudžiuvelytė Ieva <ieva.pudziuvelyte@mif.stud.vu.lt>
2021-08-18 15:10:14 +0300

    calculate.py - script that calculates distance between active sites - added

---
+++ Fusion_PyMOL/calculate.py
@@ -0,0 +1,49 @@
+#!/usr/bin/env python
+import pymol
+#from pathlib import Path
+from pymol import cmd
+import os
+import subprocess
+
+from functions import parse_parameters, color_fusion, calculate_distance
+
+# Script that colors the residues of the active site and linker in fusion
+# protein system and calculates distance between active sites
+
+# usage: pymol calculate.py -- param.txt
+# param.txt - parameter file that contains values:
+#   * fusion protein system file name (.pdb)
+#   * templates of the first and second proteins (.pdb)
+#   * lengths of the first and second proteins (.pdb)
+#   * active site residues of the first and second proteins (.pdb)
+#   * linker sequence
+#   * number of linker repeats
+#   * hex codes of the colors in the palette
+# More information about the format of parameters file can be found: https://github.com/iGEM-Vilnius/Bioinformatics/tree/master/Fusion_PyMOL
+
+class Parameters():
+    pass
+
+par = Parameters()
+par.obj_name = 'fusion'
+
+# Perform coloring
+parse_parameters.parse_parameters(sys.argv[1], par)
+
+color_fusion.color_fusion(par)
+
+# Calculate distance between active sites
+command = "grep 'MODEL' "+ par.fusion_file + " | wc -l"
+states = int(subprocess.getstatusoutput(command)[1])
+dist = [None] * states
+
+# Choose the according method for calculation
+if states > 1:
+    print("Multiple states")
+    calculate_distance.calculate_distance_multiple_states(par, dist)
+else:
+    print("One state")
+    calculate_distance.calculate_distance(par, dist)
+
+# Write distance statistics to file
+calculate_distance.write_to_file(par, dist, "./tmp/distances.txt")
