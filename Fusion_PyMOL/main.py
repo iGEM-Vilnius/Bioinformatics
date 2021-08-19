b6ca239d202c8694df9efbe821e9b0133c7d3790
Pudžiuvelytė Ieva <ieva.pudziuvelyte@mif.stud.vu.lt>
2021-08-19 09:04:28 +0300

    Script prepared for coding from the user side

--- Fusion_PyMOL/main.py
+++ Fusion_PyMOL/main.py
@@ -11,32 +11,23 @@ from functions import parse_parameters, color_fusion, calculate_distance
 # protein system
 
 # usage: pymol main.py -- param.txt
-# param.txt - parameter file that contains values of global values
+# param.txt - parameter file that contains values:
+#   * fusion protein system file name (.pdb)
+#   * templates of the first and second proteins (.pdb)
+#   * lengths of the first and second proteins (.pdb)
+#   * active site residues of the first and second proteins (.pdb)
+#   * linker sequence
+#   * number of linker repeats
+#   * hex codes of the colors in the palette
+# More information about the format of parameters file can be found: https://github.com/iGEM-Vilnius/Bioinformatics/tree/master/Fusion_PyMOL
 
+# Parse parameters
 class Parameters():
     pass
 
 par = Parameters()
 par.obj_name = 'fusion'
 
-# Perform coloring
 parse_parameters.parse_parameters(sys.argv[1], par)
 
-color_fusion.color_fusion(par)
-
-# Structural alignment with homologs
-# color_fusion.load_templates(par)
-
-# Calculate distance between active sites
-command = "grep 'MODEL' "+ par.fusion_file + " | wc -l"
-states = int(subprocess.getstatusoutput(command)[1])
-dist = [None] * states
-
-if states > 1:
-    print("Multiple states")
-    calculate_distance.calculate_distance_multiple_states(par, dist)
-else:
-    print("One state")
-    calculate_distance.calculate_distance(par, dist)
-
-calculate_distance.write_to_file(par, dist, "./tmp/distances.txt")
+# Your code below
